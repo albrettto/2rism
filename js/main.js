da26b487b7d33202ca9772a2185c14ba5e2824bf
albrettto <a.husamov@yandex.ru>
2023-11-02 22:03:07 +0500

    Реализован футер

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ const picker = new easepick.create({
     ],
     format: 'DD.MM.YY',
 });
+
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label = document.querySelector('.subscribe__label');
+inputSubscribe.addEventListener('input', ()=> {
+    if(inputSubscribe.value.trim() != '') {
+        label.classList.add('subscribe__label--top');
+    } else {
+        label.classList.remove('subscribe__label--top');
+    }
+})
